7925bca843954e652ba5bc88bb0363de98b6d16e
Siddhesh Mangela <sidthecool007@gmail.com>
2021-06-16 09:40:30 +0530

    isProduction checker added

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
+let mode = 'production';
+
+if (import.meta && import.meta.env && import.meta.env.MODE) {
+  mode = import.meta.env.MODE;
+}
+
+export const isProd = mode !== 'development';
+
 export const kebabize = (str) =>
   str.replace(
     /[A-Z]+(?![a-z])|[A-Z]/g,
